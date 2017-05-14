13d08f55e3e440bf05de66d79373424a311fa27f
Andrea Huber <andrea.huber@idealo.de>
2017-05-14 11:53:05 +0200

    Mysql database

--- src/main/java/press/turngeek/db/DBConnection.java
+++ src/main/java/press/turngeek/db/DBConnection.java
@@ -1,18 +1,18 @@
 package press.turngeek.db;
 
-import org.h2.jdbcx.JdbcDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 import javax.sql.DataSource;
 
 public class DBConnection {
 
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_CONNECTION = "jdbc:h2:mem:todo;DB_CLOSE_DELAY=-1";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_CONNECTION = "jdbc:mysql://db.f4.htw-berlin.de:3306/_hubera__todos?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+    private static final String DB_USER = "hubera";
+    private static final String DB_PASSWORD = "Frisk-Fisk1703!";
 
     public static DataSource getDatasource() {
-        JdbcDataSource datasource = new JdbcDataSource();
+        MysqlDataSource datasource = new MysqlDataSource();
         datasource.setUrl(DB_CONNECTION);
         datasource.setUser(DB_USER);
         datasource.setPassword(DB_PASSWORD);
